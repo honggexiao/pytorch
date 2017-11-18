fdcaf7c2df2d65ec8170f72e03dd7d14630afbb6
peter <peter_jiachen@163.com>
2017-11-18 23:39:23 +0800

    Fix build script
    Signed-off-by: peter <peter_jiachen@163.com>

--- torch/lib/build_libs.bat
+++ torch/lib/build_libs.bat
@@ -53,8 +53,8 @@ IF EXIST ".\tmp_install\bin" (
   copy /Y tmp_install\bin\* .
 )
 xcopy /Y /E tmp_install\include\*.* include\*.*
-xcopy /Y ..\..\aten\src\THNN\generic\THNN.h  .
-xcopy /Y ..\..\aten\src\THCUNN\generic\THCUNN.h .
+xcopy /Y tmp_install\include\THNN\generic\THNN.h  .
+xcopy /Y tmp_install\include\THCUNN\generic\THCUNN.h .
 
 goto:eof
 
